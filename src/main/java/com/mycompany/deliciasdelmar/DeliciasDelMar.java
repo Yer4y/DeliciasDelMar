62e9536620ee64a28fb1b3bf561762287cb9c9d1
yeray <yeray@Yeray>
2023-07-15 11:10:14 -0600

    Actualizacion de brandon

--- src/main/java/com/mycompany/deliciasdelmar/DeliciasDelMar.java
+++ src/main/java/com/mycompany/deliciasdelmar/DeliciasDelMar.java
@@ -24,19 +24,49 @@ public class DeliciasDelMar {
                 null,
                 botonesMenu,
                 null);
-      switch(botonint){
-          
-          case 0:
-          int numrammesa=(int)(Math.random()*12+1);
-          Mesas mesa1=new Mesas();
-          mesa1.numMesa=numrammesa;
-          
-          
-          
-          
-          
-          
-          
-      }
+        Mesas mesa[] = new Mesas[aleatorio(8, 12)];
+        switch (botonint) {
+
+            case 0:
+                llenarMesas(mesa);
+                ImprimirMesas(mesa);
+            break;
+            
+            default:
+                break;
+        }
     }
+    public static int aleatorio(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+    public static void llenarMesas(Mesas mesa[]) {
+        for (int i = 0; i < mesa.length; i++) {
+            int numMesa = 0 + 1;
+            int Cantsillas = aleatorio(2, 6);
+            boolean Estado = false;
+            int vistmar = aleatorio(0, 1);
+            DatosMesero mesero = mesa[i].mesero;
+
+            mesa[i] = new Mesas(numMesa, 
+                    Cantsillas, 
+                    Estado, 
+                    vistmar, 
+                    mesero);
+        }
+    }
+    public static void ImprimirMesas(Mesas mesa[]) {
+        for (int i = 0; i < mesa.length; i++) {
+            System.out.println("Numero mesa: " + mesa[i].getNumMesa());
+            System.out.println("Cantidad de sillas: " + mesa[i].getCantsillas());
+            System.out.println("Estado mesa: " + mesa[i].isEstado());
+            if (mesa[i].getVistmar() == 1) {
+                System.out.println("La mesa cuenta con vista al mar");
+            } else {
+                System.out.println("La mesa no cuenta con vista al mar");
+            }
+            System.out.println("Nombre de Mesero: " + mesa[i].getMesero().nombreMesero);
+
+        }
+    }
+    
 }
--- src/main/java/com/mycompany/deliciasdelmar/Mesas.java
+++ src/main/java/com/mycompany/deliciasdelmar/Mesas.java
@@ -13,8 +13,64 @@ public class Mesas {
     int numMesa;
     int Cantsillas;
     boolean Estado;
-    boolean vistmar;
-    String mesero;
+    int vistmar;
+    DatosMesero mesero;
+
+    public Mesas(int numMesa, int Cantsillas, boolean Estado, int vistmar, DatosMesero mesero) {
+        this.numMesa = numMesa;
+        this.Cantsillas = Cantsillas;
+        this.Estado = Estado;
+        this.vistmar = vistmar;
+        this.mesero = mesero;
+    }
+
+    public int getNumMesa() {
+        return numMesa;
+    }
+
+    public void setNumMesa(int numMesa) {
+        this.numMesa = numMesa;
+    }
+
+    public int getCantsillas() {
+        return Cantsillas;
+    }
+
+    public void setCantsillas(int Cantsillas) {
+        this.Cantsillas = Cantsillas;
+    }
+
+    public boolean isEstado() {
+        return Estado;
+    }
+
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    }
+
+    public int getVistmar() {
+        return vistmar;
+    }
+
+    public void setVistmar(int vistmar) {
+        this.vistmar = vistmar;
+    }
+
+    public DatosMesero getMesero() {
+        return mesero;
+    }
+
+    public void setMesero(DatosMesero mesero) {
+        this.mesero = mesero;
+    }
+    
+    
+    
+
+
+
+        
+    
     
     
 }
