105efa9225ae4dc23246092d19bd847b8ff6a0d2
yeray <yeray@Yeray>
2023-07-24 11:38:58 -0600

    Platillos hechos por daniel

---
+++ src/main/java/com/mycompany/deliciasdelmar/TipoPlatillo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.deliciasdelmar;
+
+/**
+ *
+ * @author yeray
+ */
+public enum TipoPlatillo {
+        SOPA, POSTRE, PLATILLO_PRINCIPAL, BEBIDA
+}
